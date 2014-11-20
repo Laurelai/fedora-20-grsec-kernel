f6bbe1b80958d205df1793d17348eb445c453dab
Laurelai <laurelaistorm@gmail.com>
2014-11-19 23:59:46 -0500

    Create paxsteam.sh

---
+++ paxsteam.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+paxctl -cm ~/.steam/steam/ubuntu12_32/steam
+paxctl -cm ~/.steam/steam/ubuntu12_32/steamwebhelper
+paxctl -cm ~/.steam/steam/ubuntu12_32/gameoverlayui
+paxctl -cm ~/.steam/steam/ubuntu12_32/streaming_client
+paxctl -cm ~/.steam/steam/ubuntu12_32/libavutil.so.54
