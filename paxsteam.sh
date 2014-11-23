9cc7b9daf1465986dda786c995e4db23fc893caa
Laurelai <laurelaistorm@gmail.com>
2014-11-22 21:16:42 -0500

    Update paxsteam.sh
    added tropico 5 support

--- paxsteam.sh
+++ paxsteam.sh
@@ -5,5 +5,7 @@ paxctl -cm ~/.steam/steam/ubuntu12_32/steamwebhelper
 paxctl -cm ~/.steam/steam/ubuntu12_32/gameoverlayui
 paxctl -cm ~/.steam/steam/ubuntu12_32/streaming_client
 paxctl -cm ~/.steam/steam/ubuntu12_32/libavutil.so.54
+paxctl -cm ~/.local/share/Steam/linux32/steamclient.so
 paxctl -cm ~/.local/share/Steam/steamapps/common/Kerbal\ Space\ Program/KSP.x86
 paxctl -cm ~/.local/share/Steam/steamapps/common/Kerbal\ Space\ Program/KSP_Data/Mono/x86/libmono.so
+paxctl -cm ~/.local/share/Steam/steamapps/common/Tropico\ 5/Tropico5
